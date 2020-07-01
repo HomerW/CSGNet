40b094545b31c7145340a4b8f800b7c4fcb6c8d5
HomerW <hrwalke@gmail.com>
2020-07-01 09:39:51 -0400

    tree vae and fid beginning

--- vae_tree.py
+++ vae_tree.py
@@ -1,23 +1,23 @@
-from __future__ import print_function
-import argparse
 import torch
-import torch.utils.data
-from torch import nn, optim
+from torch import nn
 from torch.nn import functional as F
-from torchvision import datasets, transforms
-from torchvision.utils import save_image
 import numpy as np
-from torch.autograd.variable import Variable
-from src.Models.loss import losses_joint
-from src.Models.models import Encoder
-from src.Models.models import ImitateJoint, ParseModelOutput
-from src.utils import read_config
-from src.utils.generators.mixed_len_generator import MixedGenerateData
-from src.utils.learn_utils import LearningRate
-from src.utils.train_utils import prepare_input_op, cosine_similarity, chamfer
 
 device = torch.device("cpu")
 
+class MLP(nn.Module):
+    def __init__(self, ind, hdim1, hdim2, odim):
+        super(MLP, self).__init__()
+        self.l1 = nn.Linear(ind, hdim1)
+        self.l2 = nn.Linear(hdim1, hdim2)
+        self.l3 = nn.Linear(hdim2, odim)
+
+    def forward(self, x):
+        x = torch.tanh(self.l1(x))
+        x = torch.tanh(self.l2(x))
+        return self.l3(x)
+
+# VAE for trees represented as dicts with "value", "left", and "right" keys
 class VAE(nn.Module):
     def __init__(self, hidden_dim, latent_dim, vocab_size, max_len):
         super(VAE, self).__init__()
@@ -27,21 +27,24 @@ class VAE(nn.Module):
         self.vocab_size = vocab_size
         self.max_len = max_len
 
+        # encoding
         self.embed = nn.Embedding(vocab_size, hidden_dim)
-        self.parent = nn.Linear(3*hidden_dim, hidden_dim)
-        self.child1 = nn.Linear(latent_dim, hidden_dim)
-        self.child2 = nn.Linear(hidden_dim, hidden_dim+1)
+        self.parent = MLP(3*hidden_dim, hidden_dim, hidden_dim, hidden_dim)
+        self.leaf = MLP(hidden_dim, hidden_dim, hidden_dim, hidden_dim)
         self.encode_mu = nn.Linear(hidden_dim, latent_dim)
         self.encode_logvar = nn.Linear(hidden_dim, latent_dim)
-        self.relu = nn.LeakyReLU(negative_slope=0.2)
-        self.decode_leaf = nn.Linear(hidden_dim, vocab_size)
-        self.decode_parent = nn.Linear(hidden_dim, 2*latent_dim+vocab_size)
 
+        # decoding
+        self.nodetype = MLP(latent_dim, hidden_dim, hidden_dim, 1) # classifies nodes as leaf or internal
+        self.decode_leaf = MLP(latent_dim, hidden_dim, hidden_dim, vocab_size)
+        self.decode_parent = MLP(latent_dim, hidden_dim, hidden_dim, 2*latent_dim+vocab_size)
 
     def encode(self, tree):
         def traverse(node):
+            # leaf
             if node["right"] is None and node["left"] is None:
-                return self.embed(node["value"])
+                return self.leaf(self.embed(node["value"]))
+            # internal
             else:
                 lchild = traverse(node["left"])
                 rchild = traverse(node["right"])
@@ -57,55 +60,60 @@ class VAE(nn.Module):
         eps = torch.randn_like(std)
         return mu + eps*std
 
-    def decode(self, z):
-        def traverse(inp):
-            final = []
-            stack = []
-            stack.append(inp)
-            while not (stack == []):
-                node_latent = stack.pop()
-                child_out = self.child2(self.relu(self.child1(node_latent)))
-                # not a leaf
-                if child_out[0] > 0 and len(stack) <= self.max_len-2:
-                    decoded = self.decode_parent(child_out[1:])
-                    value = decoded[:self.vocab_size]
-                    llatent = decoded[self.vocab_size:self.vocab_size+self.latent_dim]
-                    rlatent = decoded[self.vocab_size+self.latent_dim:]
-                    stack.append(llatent)
-                    stack.append(rlatent)
-                    final.append(value)
-                # a leaf
-                else:
-                    value = self.decode_leaf(child_out[1:])
-                    final.append(value)
-            return final
+    def decode(self, z, tree=None):
+        # returns decoded tree and a list of predicted node types
+        # (for training nodetype classifier), teacher forces tree structure
+        def traverse_train(node, code):
+            # leaf
+            if node["right"] is None and node["left"] is None:
+                return {"value": self.decode_leaf(code), "left": None, "right": None}, [self.nodetype(code)]
+            # internal
+            else:
+                par_out = self.decode_parent(code)
+                lchild, ltype = traverse_train(node["left"], par_out[:self.latent_dim])
+                rchild, rtype = traverse_train(node["right"], par_out[self.latent_dim:2*self.latent_dim])
+                return {"value": par_out[2*self.latent_dim:], "left": lchild, "right": rchild}, [self.nodetype(code)] + ltype + rtype
 
-        # UNFINISHED DOES NOT WORK
+        # returns decoded tree given just a latent code at test time
+        def traverse_test(code):
+            # leaf
+            if self.nodetype(code) < 0:
+                return {"value": self.decode_leaf(code), "left": None, "right": None}
+            # internal
+            else:
+                par_out = self.decode_parent(code)
+                lchild = traverse_test(par_out[:self.latent_dim])
+                rchild = traverse_test(par_out[self.latent_dim:2*self.latent_dim])
+                return {"value": par_out[2*self.latent_dim:], "left": lchild, "right": rchild}
 
-        return traverse(z)
+        if tree is not None:
+            return traverse_train(tree, z)
+        else:
+            return traverse_test(z)
 
     def forward(self, x):
         mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        return self.decode(z, x), mu, logvar
 
-    # Reconstruction + KL divergence losses summed over all elements and batch
-    def loss_function(self, recon_x, x, mu, logvar):
+    # Reconstruction + KL divergence losses
+    def loss_function(self, decoder_out, x, mu, logvar):
+        # returns flattened tree and target node types
         def flatten(node):
             if node["right"] is None and node["left"] is None:
-                return [node["value"]]
+                return [node["value"]], [torch.tensor([0])]
             else:
-                lchild = flatten(node["left"])
-                rchild = flatten(node["right"])
-                return [node["value"]] + lchild + rchild
+                lchild, ltype = flatten(node["left"])
+                rchild, rtype = flatten(node["right"])
+                return [node["value"]] + lchild + rchild, [torch.tensor([1])] + ltype + rtype
 
-        flat_x = torch.stack(flatten(x))
-        flat_recon_x = torch.stack(flatten(recon_x))
-        print(flat_recon_x.shape)
-        flat_x = F.pad(flat_x, (0, self.max_len-len(flat_x)), 'constant', 399)
-        flat_recon_x = F.pad(flat_recon_x, (0, 0, 0, self.max_len-len(flat_recon_x)), 'constant', 399)
+        recon_x, type_list = decoder_out
+        flat_x, target_type_list = flatten(x)
+        flat_recon_x, _ = flatten(recon_x)
 
-        CE = F.cross_entropy(flat_recon_x, flat_x, reduction='sum')
+        CE = F.cross_entropy(torch.stack(flat_recon_x), torch.stack(flat_x), reduction='sum')
+        # loss from predicting node type
+        type_loss = F.binary_cross_entropy_with_logits(torch.stack(type_list), torch.stack(target_type_list).float(), reduction='sum')
 
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -113,4 +121,5 @@ class VAE(nn.Module):
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-        return CE + KLD
+        # probably should rescale these componenents
+        return CE + type_loss + KLD
--- wake-sleep-tree.py
+++ wake-sleep-tree.py
@@ -15,7 +15,6 @@ from src.utils.generators.wake_sleep_gen_tree import WakeSleepGen
 from src.utils.learn_utils import LearningRate
 from src.utils.train_utils import prepare_input_op, cosine_similarity, chamfer, beams_parser, validity, image_from_expressions, stack_from_expressions
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from src.utils.refine import optimize_expression
 import os
@@ -24,10 +23,11 @@ import sys
 from src.utils.generators.shapenet_generater import Generator
 from vae_tree import VAE
 from tree_conversion import label_to_tree
+from ws_infer import infer_programs
 
 device = torch.device("cpu")
-inference_train_size = 50000
-inference_test_size = 1000
+inference_train_size = 1000
+inference_test_size = 100
 vocab_size = 400
 generator_hidden_dim = 256
 generator_latent_dim = 20
@@ -148,223 +148,45 @@ Trains VAE to convergence on programs from inference network
 TODO: train to convergence and not number of epochs
 """
 def train_generator(generator_net, iter):
-    labels = torch.load(f"wake_sleep_data/inference/{iter}/labels/labels_beam_width_10.pt", map_location=device)
+    labels = torch.load(f"wake_sleep_data/inference/{iter}/labels/labels.pt", map_location=device)[:100]
     trees = list(map(label_to_tree, labels))
 
     optimizer = optim.Adam(generator_net.parameters(), lr=1e-3)
 
     generator_net.train()
 
-    for epoch in range(500):
+    # nan_found = False
+
+    for epoch in range(100):
         train_loss = 0
         np.random.shuffle(trees)
-        for t in trees:
+        for i, t in enumerate(trees):
             optimizer.zero_grad()
-            recon_t, mu, logvar = generator_net(t)
-            loss = generator_net.loss_function(recon_t, t, mu, logvar)
+            decoder_out, mu, logvar = generator_net(t)
+            loss = generator_net.loss_function(decoder_out, t, mu, logvar)
             loss.backward()
-            train_loss += loss.item()
+            # for p in generator_net.parameters():
+            #     if p.grad.norm() != p.grad.norm():
+            #         nan_found = True
+            # if nan_found:
+            #     break
+            train_loss += float(loss)
             optimizer.step()
-        print(f"generator epoch {epoch} loss: {train_loss / len(labels)}")
+
+            del decoder_out, mu, logvar
+
+        # if nan_found:
+        #     break
+        print(f"generator epoch {epoch} loss: {train_loss / (len(labels) // 100)}")
 
     # sample = np.zeros((inference_train_size+inference_test_size, max_len))
-    # for i in range(0, inference_train_size+inference_test_size, batch_size):
-    #     batch_sample = torch.randn(1, batch_size, generator_latent_dim).to(device)
-    #     batch_sample = generator_net.decode(batch_sample, timesteps=labels.shape[1] - 1).cpu()
-    #     # (batch_size, timesteps)
-    #     # sample = torch.argmax(sample.permute(1, 0, 2), dim=2)
-    #     batch_sample = batch_sample.permute(1, 0, 2).max(dim=2)[1]
-    #     # remove stop token
-    #     batch_sample = batch_sample[:, :-1]
-    #     sample[i:i+batch_size] = batch_sample
-    #
+    # for i in range(inference_train_size+inference_test_size):
+    #     latent = torch.randn(generator_latent_dim).to(device)
+    #     sampled_tree = generator_net.decode(latent)
+    #     print(sampled_tree)
+
     # os.makedirs(os.path.dirname(f"wake_sleep_data/generator/{iter}/"), exist_ok=True)
     # torch.save(sample, f"wake_sleep_data/generator/{iter}/labels.pt")
-
-
-"""
-Infer programs on cad dataset
-TODO: incorporate visually guided refinement, setting the flag won't work at the moment,
-      will need to change how primitives are encoded since no longer finite set of primitives
-"""
-def infer_programs(inference_net, iter):
-    refine = False
-    save_viz = False
-
-    config = read_config.Config("config_cad.yml")
-
-    encoder_net, imitate_net = inference_net
-
-    # Load the terminals symbols of the grammar
-    with open("terminals.txt", "r") as file:
-        unique_draw = file.readlines()
-    for index, e in enumerate(unique_draw):
-        unique_draw[index] = e[0:-1]
-
-    beam_width = 5
-    config.train_size = 10000
-    imitate_net.eval()
-    imitate_net.epsilon = 0
-    parser = ParseModelOutput(unique_draw, max_len // 2 + 1, max_len,
-                              config.canvas_shape)
-    pred_expressions = []
-    pred_labels = np.zeros((config.train_size, max_len))
-    image_path = f"wake_sleep_data/inference/{iter}/images/"
-    expressions_path = f"wake_sleep_data/inference/{iter}/expressions/"
-    results_path = f"wake_sleep_data/inference/{iter}/results/"
-    labels_path = f"wake_sleep_data/inference/{iter}/labels/"
-
-    tweak_expressions_path = f"wake_sleep_data/inference/{iter}/tweak/expressions/"
-    os.makedirs(os.path.dirname(image_path), exist_ok=True)
-    os.makedirs(os.path.dirname(expressions_path), exist_ok=True)
-    os.makedirs(os.path.dirname(results_path), exist_ok=True)
-    os.makedirs(os.path.dirname(tweak_expressions_path), exist_ok=True)
-    os.makedirs(os.path.dirname(labels_path), exist_ok=True)
-
-    generator = Generator()
-
-    train_gen = generator.train_gen(
-        batch_size=config.batch_size,
-        path="data/cad/cad.h5",
-        if_augment=False)
-
-    Rs = 0
-    CDs = 0
-    Target_images = []
-    for batch_idx in range(config.train_size // config.batch_size):
-        with torch.no_grad():
-            print(f"Inferring cad batch: {batch_idx}")
-            data_ = next(train_gen)
-            labels = np.zeros((config.batch_size, max_len), dtype=np.int32)
-            one_hot_labels = prepare_input_op(labels, len(unique_draw))
-            one_hot_labels = Variable(torch.from_numpy(one_hot_labels)).to(device)
-            data = Variable(torch.from_numpy(data_)).to(device)
-
-        all_beams, next_beams_prob, all_inputs = imitate_net.beam_search(
-            [data, one_hot_labels], beam_width, max_len)
-
-        beam_labels = beams_parser(
-            all_beams, data_.shape[1], beam_width=beam_width)
-
-        beam_labels_numpy = np.zeros(
-            (config.batch_size * beam_width, max_len), dtype=np.int32)
-        Target_images.append(data_[-1, :, 0, :, :])
-        for i in range(data_.shape[1]):
-            beam_labels_numpy[i * beam_width:(
-                i + 1) * beam_width, :] = beam_labels[i]
-
-        # find expression from these predicted beam labels
-        expressions = [""] * config.batch_size * beam_width
-        for i in range(config.batch_size * beam_width):
-            for j in range(max_len):
-                expressions[i] += unique_draw[beam_labels_numpy[i, j]]
-        for index, prog in enumerate(expressions):
-            expressions[index] = prog.split("$")[0]
-
-        pred_expressions += expressions
-        predicted_images = image_from_expressions(parser, expressions)
-        target_images = data_[-1, :, 0, :, :].astype(dtype=bool)
-        target_images_new = np.repeat(
-            target_images, axis=0, repeats=beam_width)
-
-        beam_R = np.sum(np.logical_and(target_images_new, predicted_images),
-                        (1, 2)) / np.sum(np.logical_or(target_images_new, predicted_images), (1, 2))
-
-        R = np.zeros((config.batch_size, 1))
-        for r in range(config.batch_size):
-            R[r, 0] = max(beam_R[r * beam_width:(r + 1) * beam_width])
-
-        Rs += np.mean(R)
-
-        beam_CD = chamfer(target_images_new, predicted_images)
-
-        # select best expression by chamfer distance
-        best_labels = np.zeros((config.batch_size, max_len))
-        for r in range(config.batch_size):
-            best_labels[r] = beam_labels[r][np.argmin(beam_CD[r * beam_width:(r + 1) * beam_width])]
-        pred_labels[batch_idx*config.batch_size:batch_idx*config.batch_size + config.batch_size] = best_labels
-
-        CD = np.zeros((config.batch_size, 1))
-        for r in range(config.batch_size):
-            CD[r, 0] = min(beam_CD[r * beam_width:(r + 1) * beam_width])
-
-        CDs += np.mean(CD)
-
-        if save_viz:
-            for j in range(0, config.batch_size):
-                f, a = plt.subplots(1, beam_width + 1, figsize=(30, 3))
-                a[0].imshow(data_[-1, j, 0, :, :], cmap="Greys_r")
-                a[0].axis("off")
-                a[0].set_title("target")
-                for i in range(1, beam_width + 1):
-                    a[i].imshow(
-                        predicted_images[j * beam_width + i - 1],
-                        cmap="Greys_r")
-                    a[i].set_title("{}".format(i))
-                    a[i].axis("off")
-                plt.savefig(
-                    image_path +
-                    "{}.png".format(batch_idx * config.batch_size + j),
-                    transparent=0)
-                plt.close("all")
-
-                save_viz = False
-
-    print(
-        "Inferring cad average chamfer distance: {}".format(
-            CDs / (config.train_size // config.batch_size)),
-        flush=True)
-
-    if refine:
-        Target_images = np.concatenate(Target_images, 0)
-        tweaked_expressions = []
-        scores = 0
-        for index, value in enumerate(pred_expressions):
-            prog = parser.Parser.parse(value)
-            if validity(prog, len(prog), len(prog) - 1):
-                optim_expression, score = optimize_expression(
-                    value,
-                    Target_images[index // beam_width],
-                    metric="chamfer",
-                    max_iter=None)
-                print(value)
-                tweaked_expressions.append(optim_expression)
-                scores += score
-            else:
-                # If the predicted program is invalid
-                tweaked_expressions.append(value)
-                scores += 16
-
-        print("chamfer scores", scores / len(tweaked_expressions))
-        with open(
-                tweak_expressions_path +
-                "chamfer_tweak_expressions_beamwidth_{}.txt".format(beam_width),
-                "w") as file:
-            for index, value in enumerate(tweaked_expressions):
-                file.write(value + "\n")
-
-    Rs = Rs / (config.train_size // config.batch_size)
-    CDs = CDs / (config.train_size // config.batch_size)
-    print(Rs, CDs)
-    if refine:
-        results = {
-            "iou": Rs,
-            "chamferdistance": CDs,
-            "tweaked_chamfer_distance": scores / len(tweaked_expressions)
-        }
-    else:
-        results = {"iou": Rs, "chamferdistance": CDs}
-
-    with open(expressions_path +
-              "expressions_beamwidth_{}.txt".format(beam_width), "w") as file:
-        for e in pred_expressions:
-            file.write(e + "\n")
-
-    with open(results_path + "results_beam_width_{}.org".format(beam_width),
-              'w') as outfile:
-        json.dump(results, outfile)
-
-    torch.save(pred_labels, labels_path + "labels_beam_width_{}.pt".format(beam_width))
 
 """
 Get initial pretrained CSGNet inference network
@@ -409,39 +231,15 @@ def get_csgnet():
 
     return (encoder_net, imitate_net)
 
-def load_generate(iter):
-    generator = WakeSleepGen(f"wake_sleep_data/generator/{iter}/labels.pt")
-
-    train_gen = generator.get_train_data()
-
-    batch_data, batch_labels = next(train_gen)
-    f, a = plt.subplots(1, 10, figsize=(30, 3))
-    for j in range(10):
-        a[j].imshow(batch_data[-1, j, 0, :, :], cmap="Greys_r")
-        a[j].axis("off")
-    plt.savefig("10.png")
-    plt.close("all")
-
-def load_infer(iter):
-    encoder_net, imitate_net = get_csgnet()
-    print("pre loading model")
-    pretrained_dict = torch.load(f"trained_models/imitate-{iter}.pth")
-    imitate_net_dict = imitate_net.state_dict()
-    pretrained_dict = {
-        k: v
-        for k, v in pretrained_dict.items() if k in imitate_net_dict
-    }
-    imitate_net_dict.update(pretrained_dict)
-    imitate_net.load_state_dict(imitate_net_dict)
-
-    infer_programs((encoder_net, imitate_net), iter)
-
 """
 Runs the wake-sleep algorithm
 """
 def wake_sleep(iterations):
     encoder_net, imitate_net = get_csgnet()
     generator_net = VAE(generator_hidden_dim, generator_latent_dim, vocab_size, max_len).to(device)
+
+    for param in generator_net.parameters():
+        param.requires_grad = True
 
     for i in range(iterations):
         print(f"WAKE SLEEP ITERATION {i}")
@@ -454,4 +252,3 @@ def wake_sleep(iterations):
         # torch.save(generator_net.state_dict(), f"trained_models/generator-{i}.pth")
 
 wake_sleep(1)
-# load_generate(16)
